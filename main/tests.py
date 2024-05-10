fcd1e0b857e633cec899f8e9f7126aecdc4d0ae0
cl4ytn <c.nagle744@gmail.com>
2024-05-10 20:00:20 +0700

    test: verify_user

--- main/tests.py
+++ main/tests.py
@@ -24,3 +24,16 @@ class ApproveRecipeTestCase(TestCase):
 
         self.recipe.refresh_from_db()
         self.assertFalse(self.recipe.is_approved)
+
+class VerifyUserTestCase(TestCase):
+    def setUp(self):
+        self.admin = User.objects.create(username='admin', is_staff=True)
+        self.user = User.objects.create(username='user', is_verified=False)
+
+    def test_verify_user(self):
+        self.assertFalse(self.user.is_verified)
+
+        verify_user(self.user)
+
+        self.user.refresh_from_db()
+        self.assertTrue(self.user.is_verified)
