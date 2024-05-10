d96fb933de1a0aeffe703f97fafc8e5636ba507b
cl4ytn <c.nagle744@gmail.com>
2024-05-10 14:21:05 +0700

    test: testApproveRecipe

--- main/tests.py
+++ main/tests.py
@@ -1,3 +1,18 @@
 from django.test import TestCase
+from django.contrib.auth.models import User
+from .models import Recipe
+from .utils import approve_recipe
 
-# Create your tests here.
+
+class ApproveRecipeTestCase(TestCase):
+    def setUp(self):
+        self.admin = User.objects.create(username='admin', is_staff=True)
+        self.recipe = Recipe.objects.create(title='Gourmet Negroni', is_approved=False)
+
+    def testApproveRecipe(self):
+        self.assertFalse(self.recipe.is_approved)
+
+        approve_recipe(self.recipe, self.admin)
+
+        self.recipe.refresh_from_db()
+        self.assertTrue(self.recipe.is_approved)
