e3f966d6a4221abdf6a47e7a4ee44879d20a1a28
cl4ytn <c.nagle744@gmail.com>
2024-05-10 19:47:37 +0700

    feat: reject recipe url and view

--- main/urls.py
+++ main/urls.py
@@ -5,6 +5,7 @@ app_name = 'main'
 urlpatterns = [
     path('', home, name='home'),
     path('approve-recipe/<int:recipe_id>/', approve_recipe, name='approve_recipe'),
+    path('reject-recipe/<int:recipe_id>/', reject_recipe, name='reject_recipe'),
     path('recipe-list', recipe_list, name='recipe_list'),
     path('create-recipe', create_recipe, name='create_recipe'), 
     path('delete/<int:id>', delete_recipe, name='delete_recipe'),
