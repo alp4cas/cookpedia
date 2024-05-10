c150f6a054ad95091f9282ddae80d9cff2bf6b25
cl4ytn <c.nagle744@gmail.com>
2024-05-10 21:04:02 +0700

    chore: fully reset master

--- collection/urls.py
+++
@@ -1,7 +0,0 @@
-from django.urls import path
-from .views import create_or_update_collection
-
-urlpatterns = [
-    path('collections/new/', create_or_update_collection, name='create_collection'),
-    path('collections/<int:collection_id>/edit/', create_or_update_collection, name='update_collection'),
-]
