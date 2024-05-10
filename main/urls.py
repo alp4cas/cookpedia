7bd1b88fc11192f10989fe5d5db1e3132a7a0ff6
cl4ytn <c.nagle744@gmail.com>
2024-05-10 20:41:49 +0700

    feat: verify user view and url

--- main/urls.py
+++ main/urls.py
@@ -12,6 +12,5 @@ urlpatterns = [
     path('favorite/', include('favorite.urls', namespace='favorite')), 
     path('set-recipe-status/<int:recipe_id>/<str:status>/', set_recipe_status, name='set_recipe_status'),
     path('recipes/<str:param>', get_recipe, name='get_recipe'),
-    path('approve-recipe/<int:recipe_id>/', approve_recipe, name='approve_recipe'),
-    path('reject-recipe/<int:recipe_id>/', reject_recipe, name='reject_recipe'),
+    path('verify-user/<int:user_id>/', verify_user, name='verify_user'),
 ]
