2fa3e1e0e06a1bc65f65c8b55d9e016fb943a899
mariagrizelda <mariahelga423@gmail.com>
2024-05-16 18:03:58 +0700

    fix : model

--- main/urls.py
+++ main/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path
 from .views import create_recipe, home, main, delete_recipe
 
+
 urlpatterns = [
     path('', home, name='home'),
     path('main', main, name='main'),
--- user_auth/views.py
+++ user_auth/views.py
@@ -31,7 +31,7 @@ def login_user(request):
                 user = authenticate(request, username=username, password=password)
                 if user is not None:
                     login(request, user)
-                    response = HttpResponseRedirect(reverse("home:home")) 
+                    response = HttpResponseRedirect(reverse("main:main")) 
                     response.set_cookie('last_login', str(datetime.datetime.now()))
                     return response
                 else:
