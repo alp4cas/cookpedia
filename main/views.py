048fbddfe9db265745ad1f4ff4560acb499863fb
raisaafadilla <raisa.fadilla@ui.ac.id>
2024-05-24 11:05:55 +0700

    feat: error handling in create recipe

--- main/views.py
+++ main/views.py
@@ -6,7 +6,6 @@ from .models import Recipe
 from django.contrib.auth import authenticate, login
 from django.contrib.auth.decorators import login_required
 
-@login_required(login_url='user_auth:login')
 def home(request):
     return render(request, 'homepage.html')
 
@@ -24,6 +23,10 @@ def create_recipe(request):
             recipe.user = request.user
             form.save()
             return HttpResponseRedirect(reverse('main:recipe_list'))  
+        else:
+            # If the form is not valid, re-render the page with the form containing error messages
+            context = {'form': form}
+            return render(request, "create_recipe.html", context)
     else:
         form = RecipeForm()
 
