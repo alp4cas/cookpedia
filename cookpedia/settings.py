b466d08118148547fcc7abf2e77c5e09dd028b40
adhis <tiva.adhisti@ui.ac.id>
2024-05-25 21:39:32 +0700

    fix: settings

--- cookpedia/settings.py
+++ cookpedia/settings.py
@@ -124,8 +124,8 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.0/howto/static-files/
 
-STATIC_URL = '/static/'
-STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+STATIC_URL = '/public/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'public')
 STATICFILES_DIRS = [
     os.path.join(BASE_DIR, "static"), 
 ]
