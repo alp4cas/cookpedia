294f84b0ea36b2db38ad00b3a91c46ce2cb5e538
adhis <tiva.adhisti@ui.ac.id>
2024-05-25 21:47:36 +0700

    fix: urls

--- cookpedia/urls.py
+++ cookpedia/urls.py
@@ -18,6 +18,7 @@ from django.contrib import admin
 from django.urls import path,include
 from django.conf import settings
 from django.conf.urls.static import static
+from django.views.static import serve
 
 urlpatterns = [
     path('admin/', admin.site.urls),
@@ -26,4 +27,5 @@ urlpatterns = [
     path('review/',include('review.urls')),
     path('favorite/',include('favorite.urls')),
     path('collection/',include('collection.urls')),
+    path(r'^static/(?P<path>.*)$', serve,{'document_root': settings.STATIC_ROOT}),
 ]
